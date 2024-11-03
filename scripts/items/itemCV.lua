ab597c664bc79bab924758a20e5b49973bc75dc2
Israel <israelnavarreterodriguez@gmail.com>
2024-11-03 12:14:46 +0100

    Se arregla el codigo del tearflag para el CV, la solucion se basa en utiilizar el cache flag para las lagrimas para retornar el efecto deseado, sanguix puto tonto

--- scripts/items/itemCV.lua
+++ scripts/items/itemCV.lua
@@ -7,31 +7,35 @@ local CurriculumStats = {
 
 ---FUNCIONA---
 function mod:onPickupCV(player, cacheFlag)
+	
 	if player:HasCollectible(COLLECTIBLE_CV) then
+		
 		if cacheFlag == CacheFlag.CACHE_LUCK then
+		
 			player.Luck = player.Luck + CurriculumStats.LUCK * player:GetCollectibleNum(COLLECTIBLE_CV)
+		
 		end
 	end
 end
 
 mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, mod.onPickupCV)
 
-
 ---NOFUNCIONA---
-function mod:ConfussionTearEffectCV(player, TearFlags)
-	local player = Isaac.GetPlayer(0)
-	if player:HasCollectible(COLLECTIBLE_CV) then ---ATTEMPT TO CALL A NIL VALUE---
-		for _, entity in pairs(Isaac.GetRoomEntities()) do
-			local roll = math.random(100)
-			local MaxLuck = 6
-			if roll <= ((100) - 15) * player.Luck / MaxLuck + 15 then
-				player.TearFlags = player.TearFlags | player.TearFlags.ConfussionTearEffectCV
-			end
+function mod:confussionTearEffectCV(player, cacheFlag)
+	
+	if player:HasCollectible(COLLECTIBLE_CV) then
+	
+		local result = (math.random(100) <= 85 * player.Luck / 6) + 15
+	
+		if result and cacheFlag == CacheFlag.CACHE_TEARFLAG then
+	
+			player.TearFlags = player.TearFlags | TearFlags.TEAR_WIGGLE
+	
 		end
 	end
 end
 
-mod:AddCallback(ModCallbacks.MC_POST_TEAR_UPDATE, mod.ConfussionTearEffectCV)
+mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, mod.confussionTearEffectCV)
 
 
 ---POR AÑADIR: SONIDO DE WAJAJA CUANDO SE PILLE EL ITEM---
